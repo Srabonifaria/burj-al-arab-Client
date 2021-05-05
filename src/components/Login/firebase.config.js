dc9ffabee191c0ec71f3bce0e6627653564801f0
Srabonifaria <76752669+Srabonifaria@users.noreply.github.com>
2021-05-05 14:17:23 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1PQlaEOYXQCpytU9_lPAE8grTV0amgpU",
+  authDomain: "burj-al-arab23.firebaseapp.com",
+  projectId: "burj-al-arab23",
+  storageBucket: "burj-al-arab23.appspot.com",
+  messagingSenderId: "304023251456",
+  appId: "1:304023251456:web:89c5632a0e4294e7e146e5"
+};
+  export default firebaseConfig;
